c2e21ec423f83937ec4133ce4f7774f27426721a
marcos romano <mri1001@alu.ubu.es>
2021-03-15 19:05:53 +0100

    Implementación de la clase general animadora de Sprites SpriteAnimator. Issue #7

---
+++ game/Assets/Scripts/Animation/SpriteAnimator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    public Sprite[] spriteSet;
+    public float frameRate;
+    public SpriteRenderer rendererOfSprites;
+    private float nextFrameTime;
+    private int spriteIndex;
+
+    private void Awake()
+    {
+        rendererOfSprites = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSprite();
+    }
+
+    private void UpdateSprite()
+    {
+        if (Time.time - nextFrameTime > (1f / frameRate))
+        {
+            spriteIndex = ++spriteIndex % spriteSet.Length;
+            rendererOfSprites.sprite = spriteSet[spriteIndex];
+            nextFrameTime += 1f / frameRate;
+        }
+        
+    }
+}
