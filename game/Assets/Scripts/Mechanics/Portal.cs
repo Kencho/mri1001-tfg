e6880399c0df6d57a34b00e5955d96a4a348c92d
marcos romano <mri1001@alu.ubu.es>
2021-03-16 12:08:19 +0100

    Implementación de la mecánica de los protales. Issue #6

---
+++ game/Assets/Scripts/Mechanics/Portal.cs
@@ -0,0 +1,42 @@
+﻿using Platformer.Mechanics;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer.Mechanics
+{
+    public class Portal : MonoBehaviour
+    {
+        public bool portalAbbled = true;
+        public Portal partnerPortal;
+        private Collider2D portalCollider;
+
+        private void Awake()
+        {
+            portalCollider = GetComponent<Collider2D>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            manageColliderTeleport(collision);
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            portalAbbled = true;
+        }
+
+        private void manageColliderTeleport(Collision2D collision)
+        {
+            KinematicObject kineObj = collision.gameObject.GetComponent<KinematicObject>();
+            if (kineObj != null && portalAbbled)
+            {
+                partnerPortal.portalAbbled = false;
+                portalAbbled = false;
+                kineObj.transform.position = partnerPortal.transform.position;
+            }
+        }
+    }
+}
+
