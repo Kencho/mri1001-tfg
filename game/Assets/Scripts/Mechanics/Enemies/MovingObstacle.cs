a2f45884dce6c6da54191df16ef5656f29d7e3e1
marcos romano <mri1001@alu.ubu.es>
2021-04-01 19:42:56 +0200

    Creación de los obstaculos móviles y sus prefabs. Issue #17

---
+++ game/Assets/Scripts/Mechanics/Enemies/MovingObstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer.Enemies
+{
+    public class MovingObstacle : Obstacle
+    {
+        public float speed = 5f;
+
+        private void FixedUpdate()
+        {
+            this.transform.position += (Vector3) Vector2.left * speed * Time.fixedDeltaTime;
+        }
+    }
+}
+
