ad567800db2e5ecb4f3ebbdb14a9255e9e25cb6d
marcos romano <mri1001@alu.ubu.es>
2021-04-02 12:34:39 +0200

    Intento de crear fabricas de obstáculos móviles. Issue #17

---
+++ game/Assets/Scripts/Mechanics/Enemies/Obstacle Fabrics/ObstacleFabric.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer.Enemies
+{
+    public interface ObstacleFabric
+    {
+        void SpawnObstacle();
+
+    }
+}
+
