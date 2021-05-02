75225a079f48491d4c522c2ff677190f30116031
Marcos_Romano <mri1001@alu.ubu.es>
2021-05-02 13:36:45 +0200

    Creados Gizmos que faciliten la identificación de los portales. Issue #37

---
+++ game/Assets/Scripts/Gizmos/WallGizmo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGizmo : MonoBehaviour
+{
+    private void OnDrawGizmosSelected()
+    {
+        Bounds wall_collider = GetComponent<Collider2D>().bounds;
+
+        if (wall_collider != null)
+        {
+            Gizmos.color = new Vector4(0, 0, 1, 0.6f);
+            Gizmos.DrawCube(wall_collider.center, 2 * wall_collider.extents);
+        }
+    }
+}
