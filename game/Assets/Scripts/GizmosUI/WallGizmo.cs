d2d588ddad763a8f583fa61e77e61c2169f3b3a7
Marcos_Romano <mri1001@alu.ubu.es>
2021-05-21 11:57:11 +0200

    Refactorización de WallGizmo. Issue #69

--- game/Assets/Scripts/GizmosUI/WallGizmo.cs
+++ game/Assets/Scripts/GizmosUI/WallGizmo.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallGizmo : MonoBehaviour
+namespace Platformer.GizmosUI
 {
-    private void OnDrawGizmosSelected()
+    public class WallGizmo : MonoBehaviour
     {
-        Bounds wall_collider = GetComponent<Collider2D>().bounds;
-
-        if (wall_collider != null)
+        private void OnDrawGizmosSelected()
         {
-            Gizmos.color = new Vector4(0, 0, 1, 0.6f);
-            Gizmos.DrawCube(wall_collider.center, 2 * wall_collider.extents);
+            Bounds wall_collider = GetComponent<Collider2D>().bounds;
+
+            if (wall_collider != null)
+            {
+                Gizmos.color = new Vector4(0, 0, 1, 0.6f);
+                Gizmos.DrawCube(wall_collider.center, 2 * wall_collider.extents);
+            }
         }
     }
+
 }
