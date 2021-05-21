96ccc85ac874651a2aba33c150af17556ba44ee7
Marcos_Romano <mri1001@alu.ubu.es>
2021-05-21 12:25:35 +0200

    Refactorización de GravityInverter. Issue #69

--- game/Assets/Scripts/Mechanics/GravityModifiers/GravityInverter.cs
+++ game/Assets/Scripts/Mechanics/GravityModifiers/GravityInverter.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Platformer.Mechanics
+namespace Platformer.Mechanics.GravityModifiers
 {
     public class GravityInverter : MonoBehaviour
     {
