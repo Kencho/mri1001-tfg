c269251ef17a92b7f973454e56f82a0753ddc35d
Marcos_Romano <mri1001@alu.ubu.es>
2021-05-22 18:43:01 +0200

    Documentación de la case Portal. Issue #70

--- game/Assets/Scripts/Mechanics/Portal.cs
+++ game/Assets/Scripts/Mechanics/Portal.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 
 namespace Platformer.Mechanics
 {
+    /// <summary>
+    /// Class that teleports KinematicObjects to other portal
+    /// </summary>
     public class Portal : MonoBehaviour
     {
         public bool portalEnabled = true;
+        /// <summary>
+        /// Portal where the KinematicObjects will be teleported
+        /// </summary>
         public Portal partnerPortal;
         private Collider2D portalCollider;
 
@@ -24,6 +30,10 @@ namespace Platformer.Mechanics
             portalEnabled = true;
         }
 
+        /// <summary>
+        /// Teleport KinematicObjects if entered in contact with the portal
+        /// </summary>
+        /// <param name="collision"></param>
         private void manageColliderTeleport(Collision2D collision)
         {
             KinematicObject kineObj = collision.gameObject.GetComponent<KinematicObject>();
