285635b5656027bdbbdfb8335a3d9ccaf81e0d1b
Marcos_Romano <mri1001@alu.ubu.es>
2021-05-22 09:44:13 +0200

    Documentación de las clases delnamespace Platformer.Gameplay.GameEvents. Issue #70

--- game/Assets/Scripts/Gameplay/GameEvents/SetGameInitialState.cs
+++ game/Assets/Scripts/Gameplay/GameEvents/SetGameInitialState.cs
@@ -3,6 +3,9 @@ using Platformer.Model;
 
 namespace Platformer.Gameplay.GameEvents
 {
+    /// <summary>
+    /// Event that notyfis the GameController to set the initial state
+    /// </summary>
     public class SetGameInitialState : Simulation.Event<SetGameInitialState>
     {
         private const float WAIT_TIME_UNITL_EXECUTE = 3.5f;
