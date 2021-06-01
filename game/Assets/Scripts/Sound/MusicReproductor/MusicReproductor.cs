fac0cc890f4a4b6dfa2cbc104766a8c8f008846c
Marcos_Romano <mri1001@alu.ubu.es>
2021-06-01 20:52:02 +0200

    añadida canción al menú principal y de opciones. Issue #83

---
+++ game/Assets/Scripts/Sound/MusicReproductor/MusicReproductor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Platformer.Sound.MusicReproductor
+{
+    /// <summary>
+    /// Plays a son in loop
+    /// </summary>
+    public class MusicReproductor : MonoBehaviour
+    {
+        protected AudioSource audioSource;
+
+        protected virtual void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+        }
+    }
+}
+
---
+++ game/Assets/Scripts/Sound/MusicReproductor/MusicReproductorInterScenes.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Platformer.Sound.MusicReproductor
+{
+    /// <summary>
+    /// MusicReproductor that reproduces the same song between scenes
+    /// </summary>
+    public class MusicReproductorInterScenes : MusicReproductor
+    {
+        private const string SONG_TIME_VARIABLE_NAME = "SongTime";
+
+        protected override void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+            LoadSongTime();
+            audioSource.Play();
+        }
+
+        public void SaveSongTime()
+        {
+            PlayerPrefs.SetFloat(SONG_TIME_VARIABLE_NAME, audioSource.time);
+        }
+
+        private void LoadSongTime()
+        {
+            audioSource.time = PlayerPrefs.GetFloat(SONG_TIME_VARIABLE_NAME, 0);
+        }
+
+        /// <summary>
+        /// Makes sure the next time the song will reproduce it will start reproducing at the begining
+        /// </summary>
+        public void ResetSongTime()
+        {
+            PlayerPrefs.DeleteKey(SONG_TIME_VARIABLE_NAME);
+        }
+    }
+}
+
